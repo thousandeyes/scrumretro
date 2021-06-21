16b15c670231035c2cbb08035f20068413391794
Filip T. Wieland <fwieland@thousandeyes.com>
2021-06-21 08:39:10 +0100

    Initial commit

---
+++ client/pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="menu">
+    <h1>🃏 lol scrum retro</h1>
+    <nuxt-link to="/play">Play</nuxt-link>
+    <nuxt-link to="/host">Host Game</nuxt-link>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+  };
+</script>
+
+<style scoped>
+  .menu {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    width: 100%;
+    max-width: 600px;
+    margin: 0 auto;
+  }
+  .menu > a {
+    display: block;
+    padding: 15px;
+    margin: 5px 0;
+    background-color: #d1d1d1;
+    text-align: center;
+    text-decoration: none;
+  }
+  .menu > a:hover {
+    background-color: #ffc583;
+  }
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+const { wsApi } = require('./.settings/secrets.json');
+
+export default {
+    head: {
+        title: 'lol scrum retro',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: 'wow such serverless' },
+        ],
+    },
+
+    srcDir: 'client/',
+
+    modules: [
+        '@nuxt/http',
+    ],
+    buildModules: [
+        '@nuxt/typescript-build',
+    ],
+
+    render: {
+        compressor: false,
+    },
+
+    css: [
+        '~/assets/style.css',
+    ],
+
+    publicRuntimeConfig: {
+        websocketUrl: wsApi[process.env.LAMBDA_ENV || 'dev'],
+    },
+};
